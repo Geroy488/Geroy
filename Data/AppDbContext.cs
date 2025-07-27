f95265234ebc4f185670732cdbfb85fdf1b9301c
Geroy488 <mayecha302@gmail.com>
2025-07-27 09:33:48 +0800

    initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static Geroy.Models.InventoryModel;
+
+namespace Geroy.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+               : base(options)
+        {
+
+        }
+        public DbSet<Campus> Campuses { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/InventoryModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Geroy.Models
+{
+    public class InventoryModel
+    {
+        public class Campus
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int ID { get; set; }
+
+            public string? CampusName { get; set; }
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string? Username { get; set; }
+            public string? Password { get; set; }
+        }
+    }
+}
